71b2b1ac474a82f929beb1f4dc8bf8f390190609
Gyanu <gyanusherchan@outlook.com>
2022-11-05 19:23:58 +1100

    dynamic card functionality

---
+++ src/SearchBox.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SearchBox = ()=>{
+    return(
+        <div className='pa2'>
+            <input type="search" 
+            placeholder="search robofriend" 
+            className='pa3 ba b--green bg-lightest-blue'
+            />
+        </div>
+    )
+}
+
+export default SearchBox
