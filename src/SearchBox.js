955891c060cab4d0d1c148b1bf77f2fc571a304a
Gyanu <gyanusherchan@outlook.com>
2022-11-07 17:39:34 +1100

    implement searchbox functionality

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-const SearchBox = ()=>{
+const SearchBox = ({searchfield, searchChange})=>{
     return(
         <div className='pa2'>
             <input type="search" 
             placeholder="search robofriend" 
             className='pa3 ba b--green bg-lightest-blue'
+            onChange = {searchChange}
             />
         </div>
     )
